b7447047de10037303181e496602ba964cf951dc
Emanuel Solis <esolis6114@gmail.com>
2024-10-11 15:51:18 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Server Side Stack
+[Axum](https://docs.rs/axum/latest/axum/) - web server library
+
+[Askama](https://djc.github.io/askama/askama.html) - templating engine
+
+# DevOps 
+[Docker](https://docs.docker.com/) - containerization
+
+[Traefik](https://doc.traefik.io/) - Reverse Proxy, TLS Cert Renewal
+
+[Watchtower](https://containrrr.dev/watchtower/) - Automated deployments
+
+[Github Actions](https://docs.github.com/en/actions) - CI/CD
+
+[UptimeRobot](https://uptimerobot.com/) - basic monitoring
+
+# Frontend Stack
+[HTMX](https://htmx.org/) 
---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use askama_axum::Template;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::{get, post},
+    Router,
+};
+use tower_http::{services::ServeDir, trace::TraceLayer};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root))
+        .nest_service("/static", ServeDir::new("static"));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Template)]
+#[template(path = "base.html")]
+struct BaseTemplate;
+
+struct HtmlTemplate<T>(T);
+
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {err}"),
+            )
+                .into_response(),
+        }
+    }
+}
+
+// basic handler that responds with a static string
+async fn root() -> impl IntoResponse {
+    HtmlTemplate(BaseTemplate)
+}
