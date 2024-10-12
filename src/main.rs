1d53b53695c3a012b1b0ab1e70556fe775da44ae
Emanuel Solis <esolis6114@gmail.com>
2024-10-12 14:04:15 -0400

    fix(502 Bad Gateway): Fixes 502 Error
    My blog server was listening to connections on 127.0.0.1:8080, but
    requests were coming in on 172.18.0.2:8080 so they were not being
    served. This PR fixes the issue by listening to all connections via
    0.0.0.0.
    
    Adds tracing layer to service via tower_http
    
    Creates new dev Dockerfile for local development

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,11 @@ async fn main() {
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/", get(root))
+        .layer(TraceLayer::new_for_http())
         .nest_service("/static", ServeDir::new("static"));
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
-        .await
-        .unwrap();
+    // run our app with hyper, listening globally on port 8080
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
